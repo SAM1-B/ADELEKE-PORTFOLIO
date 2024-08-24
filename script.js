6376aa54ec526bcb4aa69a83f5de30a4f4dce70b
samuel <babatundeola876@gmail.com>
2024-08-24 22:40:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var image1= document.getElementById('image1');
+var work= document.getElementById('work');
+var showOne= document.querySelector('.show1');
+var showTwo= document.querySelector('.show2');
+var showThree= document.querySelector('.show3');
+var showFour= document.querySelector('.show4');
+var showFive= document.querySelector('.show5');
+
+
+
+
+
+function show1() {
+    showOne.classList.toggle('use1')
+    showOne.style.zindex='1000px'
+}
+function show2() {
+    showTwo.classList.toggle('use1')
+    showTwo.style.zindex='1200px'
+}
+function show3() {
+    showThree.classList.toggle('use1')
+    showThree.style.zindex='1300px'
+}
+function show4() {
+    showFour.classList.toggle('use1')
+    showFour.style.zindex='1400px'
+}
+function show5() {
+    showFive.classList.toggle('use1')
+    showFive.style.zindex='1500px'
+}
+
+
+
+
+
+
